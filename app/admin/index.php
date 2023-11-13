ddd5fc8a5d33459e9bf3b8eff7fd0a2496234082
bagusSatriaP2408 <bagusputraanugrah123@gmail.com>
2023-11-13 17:19:18 +0700

    menambahkan halaman admin & manajer (belum sempurna)

--- app/admin/index.php
+++ app/admin/index.php
@@ -14,13 +14,26 @@ if (!isset($_SESSION['login'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
+    <link rel="stylesheet" href="../../assets/styles/style_admin.css" />
 </head>
 <body>
+    <div class="khusus">
+        <div class="kiri">
+            <div>
+                <img class="logo" src="logo.png" alt="logo">
+            </div>
+            <div class="profile">
+                <img src="profile.png" alt="profile">
+                <h3>Admin</h3>
+            </div>
+            <div class="dashboard"><h3>Dashboard</h3></div>
+            <div class="infoakun"><h3>Info Akun</h3></div>
+            <div class="infobarang"><h3>Info Barang</h3></div>    
+            <div class="transaksi"><h3>Info Transaksi</h3></div>
+        </div>
+        <div class="kanan">
 
-    <h1>Ini Halaman Admin</h1>
-
-    <a href="../logout.php">Logout</a>
-
-    
+        </div>
+    </div>
 </body>
 </html>
--- app/manajer/index.php
+++ app/manajer/index.php
@@ -13,13 +13,52 @@ if (!isset($_SESSION['login'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Manajer</title>
+    <title>Manager</title>
+    <link rel="stylesheet" href="../../assets/styles/style_manajer.css" />
 </head>
 <body>
-
-    <h1>Ini Halaman Manajer</h1>
-
-    <a href="../logout.php">Logout</a>
-    
+    <div class="header">
+        <div class="profile">
+            <img class="pfl" src="profile.png" alt="profile">
+        </div>
+        <div class="logo">
+            <img src="logo.png" alt="logo">
+        </div>
+        <div class="search">
+            <img class="src" src="search.png" alt="search">
+        </div>
+    </div>
+    <div class="grafik">
+        GRAFIK
+    </div>
+    <div class="rekap">
+        <table border="1" class="table" width="1000">
+            <tr>
+                <td>No.</td>
+                <td>Nama</td>
+                <td>Keterangan</td>
+            </tr>
+            <tr>
+                <td>p</td>
+                <td>p</td>
+                <td>p</td>
+            </tr>
+            <tr>
+                <td>p</td>
+                <td>p</td>
+                <td>p</td>
+            </tr>
+            <tr>
+                <td>p</td>
+                <td>p</td>
+                <td>p</td>
+            </tr>
+            <tr>
+                <td>p</td>
+                <td>p</td>
+                <td>p</td>
+            </tr>
+        </table>
+    </div>
 </body>
 </html>
