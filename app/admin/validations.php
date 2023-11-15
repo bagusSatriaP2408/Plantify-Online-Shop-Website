f51e8fc7f76ae6b26a72de720fa8e8c9d2d6ea5c
bagusSatriaP2408 <bagusputraanugrah123@gmail.com>
2023-11-15 22:41:42 +0700

    menambahkan ubah produk dan hapus produk

--- app/admin/validations.php
+++ app/admin/validations.php
@@ -11,6 +11,11 @@ function checkAlphabet($field) {
     return preg_match($pattern, $field);
 }
 
+function checkAlphaNumeric($field) {
+    $pattern = "/^([a-zA-Z]+[0-9]+)|([0-9]+[a-zA-Z]+)$/";
+    return preg_match($pattern, $field);
+}
+
 function checkNumeric($field) {
     $pattern = "/^[0-9]+$/";
     return preg_match($pattern, $field);
@@ -54,61 +59,59 @@ function validateAlamat(&$errors, $address) {
 }
 
 function validasiTambahProduk (&$errors, $inputan) {
-    if (checkRequired($inputan['nama_produk']) || checkRequired($inputan['harga']) || checkRequired($inputan['stok']) || $inputan['kategori'] == 0 || $inputan['supplier'] == 0) {
+
+    $nama_produk = htmlspecialchars($inputan['nama_produk']);
+    $harga = htmlspecialchars($inputan['harga']);
+    $stok = htmlspecialchars($inputan['stok']);
+    $kategori = $inputan['kategori'];
+    $sup = $inputan['supplier'];
+
+    $stat = DB->prepare("SELECT nama_produk FROM produk WHERE nama_produk = :nama_produk");
+    $stat->execute(array(":nama_produk" => $nama_produk));
+
+    if (checkRequired($nama_produk) || checkRequired($harga) || checkRequired($stok) || $kategori == 0 || $sup == 0) {
         $errors['error'] = "data produk tidak boleh ada yang kosong";
-    } else if (!checkAlphabet($inputan['nama_produk'])) {
-        $errors['error'] = "nama produk harus berupa alfabet";
-    } else if (!checkNumeric($inputan['harga'])) {
+    } else if (!checkAlphaNumeric($nama_produk) && !checkAlphabet($nama_produk)) {
+        $errors['error'] = "nama produk tidak boleh mengandung simbol";
+    } else if ($stat->rowCount() > 0) {
+        $errors["error"] = "nama produk sudah ada";
+    } else if (!checkNumeric($harga)) {
         $errors['error'] = "harga produk harus berupa angka";
-    } else if (!checkNumeric($inputan['stok'])) {
+    } else if (!checkNumeric($stok)) {
         $errors['error'] = "stok produk harus berupa angka";
     }
 }
 
-function upload() {
-
+function uploadGambar(&$errors) {
+    
     $namaFile = $_FILES["gambar"]["name"];
-    $ukuranFile = $_FILES["gambar"]["size"];
     $error = $_FILES["gambar"]["error"];
     $tmpName = $_FILES["gambar"]["tmp_name"];
-
+    
     // cek apakah tidak ada gambar yang diupload
     if ($error === 4) {
-        echo "<script>
-                alert('pilih gambar terlebih dahulu');
-            </script>";
+        $errors['error'] = "pilih gambar terlebih dahulu";
         return false;
     }
-
+    
     // cek apakah yang diupload adalah gambar (cek ekstensi)
     $ekstensiGambarValid = ['jpg', 'jpeg', 'png'];
     $ekstensiGambar = explode('.', $namaFile); // memecah string dengan delimiter .
     $ekstensiGambar = strtolower(end($ekstensiGambar)); // ambil indeks terakhir
-
+    
     // cek apakah ekstensi ada di ekstensigambarvalid
     if (!in_array($ekstensiGambar, $ekstensiGambarValid)) {
-        echo "<script>
-                alert('yang diupload bukan gambar');
-            </script>";
+        $errors['error'] = "ekstensi gambar tidak valid";
         return false;
     }
-
-    // cek jika ukurannya terlalu besar
-    if ($ukuranFile > 1000000) {
-        echo "<script>
-                alert('ukuran gambar terlalu besar');
-            </script>";
-        return false;
-    }
-
-    // lolos pengecekan, gambar siap diupload
+    
     // generate nama gambar baru
     $namaFileBaru = uniqid();
     $namaFileBaru .= '.';
     $namaFileBaru .= $ekstensiGambar;
 
-    move_uploaded_file($tmpName, 'img/' . $namaFileBaru);
-
+    move_uploaded_file($tmpName, "../../assets/img/" . $namaFileBaru); 
+    
     return $namaFileBaru;
 
 }
